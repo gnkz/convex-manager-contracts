7619d601ee9d13a60e98480cf1f749ef74e0286d
gnkz <gnkz.dev@gmail.com>
2021-08-09 21:52:12 -0400

    use typescript

---
+++ hardhat.config.ts
@@ -0,0 +1,43 @@
+import dotenv from "dotenv";
+
+import "@nomiclabs/hardhat-waffle";
+import '@nomiclabs/hardhat-ethers'
+
+import { task, HardhatUserConfig } from "hardhat/config";
+
+dotenv.config();
+
+task("accounts", "Prints the list of accounts", async (_, hre) => {
+  const accounts = await hre.ethers.getSigners();
+
+  for (const account of accounts) {
+    console.log(account.address);
+  }
+});
+
+const config: HardhatUserConfig = {
+  solidity: {
+    version: "0.8.4",
+    settings: {
+       optimizer: {
+           enabled: true,
+           runs: 1000000,
+       } ,
+    },
+  },
+  networks: {
+    mainnet: {
+      url: `https://mainnet.infura.io/v3/${process.env.INFURA_PROJECT_ID}`,
+      accounts: {
+        mnemonic: process.env.MNEMONIC,
+      },
+    },
+    hardhat: {
+      forking: {
+        url: `https://eth-mainnet.alchemyapi.io/v2/${process.env.ALCHEMY_API_KEY}`,
+      },
+    },
+  }
+};
+
+export default config;
